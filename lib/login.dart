6f60708a5b9b0bea5145d337b49e94f55379a334
lacrim9423 <mohamedmedjabri@gmail.com>
2023-05-05 16:02:44 +0100

    j

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sos_restau/home.dart';
 import 'package:sos_restau/register.dart';
 
 class LoginPage extends StatefulWidget {
@@ -19,6 +22,8 @@ class _LoginPageState extends State<LoginPage> {
         email: _emailController.text.trim(),
         password: final_motdepasseController.text.trim(),
       );
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()));
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
